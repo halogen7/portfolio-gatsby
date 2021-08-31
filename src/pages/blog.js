9f530e1019fd7f28a51bc66d5c5529915fea0b8b
Mars <gmar.gilb@gmail.com>
2021-08-31 18:08:13 -0400

    Gatsby tutorial p5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/Layout/layout";
 
 const BlogPage = ({ data }) => {
 	return (
 		<Layout pageTitle={"My Blog Posts"}>
-			<ul>
-				{data.allFile.nodes.map((post) => (
-					<li key={post.name}>
-						<h1>{post.name}</h1>
-						<p>{post.modifiedTime}</p>
-					</li>
-				))}
-			</ul>
+			{data.allMdx.nodes.map((post) => (
+				<article key={post.id}>
+					<h2>{post.frontmatter.title}</h2>
+					<p>Posted on {post.parent.modifiedTime}</p>
+					<MDXRenderer>{post.body}</MDXRenderer>
+				</article>
+			))}
 		</Layout>
 	);
 };
 
 export const query = graphql`
 	query {
-		allFile(filter: { ext: { eq: ".mdx" } }) {
+		allMdx(sort: { fields: frontmatter___date, order: DESC }) {
 			nodes {
-				name
-				modifiedTime
+				frontmatter {
+					title
+				}
+				id
+				body
+				parent {
+					... on File {
+						modifiedTime(formatString: "MMMM D, YYYY")
+					}
+				}
 			}
 		}
 	}
